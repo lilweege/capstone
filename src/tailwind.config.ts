bcc935dc0f3bb59a95831bf975e1fe81cde6e063
Mohsin <76669754+MohammadMohsinKhan@users.noreply.github.com>
2025-01-22 11:43:54 -0500

    got rid of bloat

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -6,7 +6,7 @@ export default {
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
+    "./frontend/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
